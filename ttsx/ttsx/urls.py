7c1353999a8bf1caf8c91fc226952693cdaf10b9
why <email@example.com>
2017-01-04 18:54:47 +0800

    add init

--- ttsx/ttsx/urls.py
+++ ttsx/ttsx/urls.py
@@ -17,6 +17,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^shopping/', include('shopping.urls', namespace='shopping')),
+    url(r'^userCenter/', include('userCenter.urls', namespace='userCenter')),
+    url(r'^favicon.ico$', RedirectView.as_view(url=r'static/favicon.ico')),
 ]+ static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
