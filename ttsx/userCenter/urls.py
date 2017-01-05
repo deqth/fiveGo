21a1c6a81ba1d2b7f7b302620734628f5390e19f
wei <403643884@qq.com>
2017-01-05 20:16:30 +0800

    显示静态页面

--- ttsx/userCenter/urls.py
+++ ttsx/userCenter/urls.py
@@ -1,7 +1,10 @@
 #coding=utf-8
 from django.conf.urls import url
 from userCenter import views
+from userCenter import *
 urlpatterns = [
-   url(r'^register/$', views.register),
-   url(r'^register/create/$', views.createUser),
+    url(r'^register/$', views.register),
+    # url(r'^register/create/$', views.createUser),
+    url(r'^cart/$',views.cart),
+
  ]
--- ttsx/userCenter/views.py
+++ ttsx/userCenter/views.py
@@ -4,9 +4,6 @@ from django.contrib.auth.models import User
 def register(request):
     return render(request,'userCenter/register.html')
 
-def createUser(request):
-    user = User.objects.create_user(username=request.POST['name'],password=request.POST['pwd'],email=request.POST['email'])
-    user.save()
-    return HttpResponseRedirect('userCenter/login.html')
 
-
+def cart(request):
+    return render(request,'userCenter/cart.html')
