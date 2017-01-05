5e00ae737fb7dfdbfc1e7a76d448008ae867bf6e
why <email@example.com>
2017-01-05 20:11:16 +0800

    整合注册登录

--- ttsx/userCenter/urls.py
+++ ttsx/userCenter/urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls import url
 from userCenter import views
 urlpatterns = [
-   url(r'^login/$', views.login_split,{'get':views.login,'post':views.toLogin},name='login'),
-   url(r'^register/$', views.register_split,{'get':views.register,'post':views.createUser},name='register'),
+   url(r'^login/$', views.login_register_split,{'get':views.login,'post':views.toLogin},name='login'),
+   url(r'^register/$', views.login_register_split,{'get':views.register,'post':views.createUser},name='register'),
  ]
--- ttsx/userCenter/views.py
+++ ttsx/userCenter/views.py
@@ -1,33 +1,33 @@
 #coding=utf-8
 from django.shortcuts import render
-from django.http import HttpResponse,Http404,HttpResponseRedirect,JsonResponse
+from django.http import Http404,JsonResponse
 from django.contrib import auth
 from django import forms
 
-
-class ttsxUser(forms.Form):
-    name = forms.CharField(max_length=20,min_length=3)
-    pwd = forms.CharField(max_length=20,min_length=6)
-    email = forms.EmailField(max_length=30,min_length=3)
-
-def register_split(request,get,post):
-
+def login_register_split(request,get,post):
     if request.method == 'GET':
         return get(request)
     elif request.method == 'POST':
         return post(request)
     else:
         raise Http404
-
+#注册页面
 def register(request):
     return render(request,'userCenter/register.html')
 
+#验证提交信息的正确性
+class ttsxUser(forms.Form):
+    name = forms.CharField(max_length=20,min_length=3)
+    pwd = forms.CharField(max_length=20,min_length=6)
+    email = forms.EmailField(max_length=30,min_length=3)
+#注册用户
 def createUser(request):
     form = ttsxUser(request.POST)
     isCreate = False
     if form.is_valid():
         info = form.cleaned_data
         try:
+            #用户名已经存在抛异常
             user = User.objects.create_user(username=info['name'],password=info['pwd'],email=info['email'])
             user.save()
         except Exception,e:
@@ -36,18 +36,12 @@ def createUser(request):
             isCreate = True
     return JsonResponse({'register':isCreate})
 
-def login_split(request,get,post):
-    if request.method == 'GET':
-        return get(request)
-    elif request.method == 'POST':
-        return post(request)
-    else:
-        raise Http404
-
+#登陆页面
 def login(request):
     return  render(request,'userCenter/login.html')
-from django.contrib.auth.models import User
 
+#用户登陆
+from django.contrib.auth.models import User
 def toLogin(request):
     username = request.POST.get('username', '')
     pwd = request.POST.get('pwd', '')
