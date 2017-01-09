c2f90e3e1fb9eb68f6cf4f2f86fe726b1b3e7c01
why <email@example.com>
2017-01-09 18:45:31 +0800

    增加admin里表的说明

--- ttsx/shopping/models.py
+++ ttsx/shopping/models.py
@@ -2,17 +2,20 @@
 from django.db import models
 
 class GoodsInfo(models.Model):
-    title = models.CharField(max_length=20)    #商品名字
-    type = models.CharField(max_length=20)     #商品类型
-    price = models.DecimalField(max_digits=5,decimal_places=2)            #商品价格
-    unit = models.CharField(max_length=20)     #商品单位
-    smallImg = models.ImageField()#商品图片
-    info = models.CharField(max_length=200)   #商品简介
-    info_detail = models.CharField(max_length=3000) #商品详情
-    bigImg = models.ImageField()#商品图片
-    cliNum = models.IntegerField()#点击量
+    title = models.CharField(max_length=20,verbose_name='商品信息')    #商品名字
+    type = models.CharField(max_length=20,verbose_name='商品类型')     #商品类型
+    price = models.DecimalField(max_digits=5,decimal_places=2,verbose_name='商品价格')            #商品价格
+    unit = models.CharField(max_length=20,verbose_name='商品单位')     #商品单位
+    smallImg = models.ImageField(verbose_name='商品小图')#商品图片
+    info = models.CharField(max_length=200,verbose_name='商品简介')   #商品简介
+    info_detail = models.TextField(max_length=3000,verbose_name='商品详情') #商品详情
+    bigImg = models.ImageField(verbose_name='商品大图')#商品图片
+    cliNum = models.IntegerField(verbose_name='点击量')#点击量
     class Meta():
         db_table = 'goods'
+        verbose_name = '商品信息'
+        verbose_name_plural = verbose_name
+
     #后台admin站点添加数据显示名字而不是对象
     def __str__(self):
         #__unicode__
--- ttsx/userCenter/models.py
+++ ttsx/userCenter/models.py
@@ -4,23 +4,25 @@ from shopping.models import *
 from django.contrib.auth.models import User
 
 
-
 class cart(models.Model):
-    num = models.IntegerField()
+    num = models.IntegerField(verbose_name='数量')
     user = models.ForeignKey(User)
     goods_info = models.ForeignKey(GoodsInfo)
     class Meta:
         db_table = 'cart'
+        verbose_name = '购物车'
+        verbose_name_plural = verbose_name
 class address_info(models.Model):
-    receiver = models.CharField(max_length=15)
-    tel = models.CharField(max_length=15)
-    email = models.EmailField()
-    zipcode = models.CharField(max_length=10)
-    address = models.CharField(max_length=50)
+    receiver = models.CharField(max_length=15,verbose_name='收件人')
+    tel = models.CharField(max_length=15,verbose_name='电话号码')
+    email = models.EmailField(verbose_name='邮箱')
+    zipcode = models.CharField(max_length=10,verbose_name='邮编')
+    address = models.CharField(max_length=50,verbose_name='地址')
     user = models.ForeignKey(User)
     isDelete = models.BooleanField(default=0)
-    receiver = models.CharField(max_length=15)
     class Meta:
+        verbose_name = '地址信息'
+        verbose_name_plural = verbose_name
         db_table = 'address_info'
     # 后台admin站点添加数据显示名字而不是对象
     def __str__(self):
@@ -29,11 +31,13 @@ class address_info(models.Model):
 
 class OrderInfo(models.Model):
     user = models.ForeignKey(User)
-    state = models.BooleanField()
-    total = models.DecimalField(max_digits=5,decimal_places=2)
-    ordernum = models.CharField(max_length=20)
-    bpub_date = models.DateTimeField()
+    state = models.BooleanField(verbose_name='支付状态')
+    total = models.DecimalField(max_digits=5,decimal_places=2,verbose_name='支付金额')
+    ordernum = models.CharField(max_length=20,verbose_name='订单号')
+    bpub_date = models.DateTimeField(verbose_name='订单日期')
     class Meta:
+        verbose_name = '订单'
+        verbose_name_plural = verbose_name
         db_table = 'OrderInfo'
     def __str__(self):
         return str(self.total)
@@ -41,8 +45,10 @@ class OrderInfo(models.Model):
 class OrderDetailInfo(models.Model):
     order = models.ForeignKey(OrderInfo)
     goods = models.ForeignKey(GoodsInfo)
-    goods_price = models.DecimalField(max_digits=5,decimal_places=2)
-    count = models.DecimalField(max_digits=5,decimal_places=2)
+    goods_price = models.DecimalField(max_digits=5,decimal_places=2,verbose_name='商品价格')
+    count = models.DecimalField(max_digits=5,decimal_places=2,verbose_name='商品数量')
     class Meta:
+        verbose_name = '订单详情'
+        verbose_name_plural = verbose_name
         db_table = 'OrderDetailInfo'
 
