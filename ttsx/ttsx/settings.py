a7d363605c2a75f92ca88db1e4425060c8ff76ef
yuhao15 <2310839536@qq.com>
2017-01-12 19:03:59 +0800

    allok!!!

--- ttsx/ttsx/settings.py
+++ ttsx/ttsx/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(330%6@cz*$&&&is&g=qey8l&k-csg41g5#=^ao2a_no%qr5ee'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*',]
 
